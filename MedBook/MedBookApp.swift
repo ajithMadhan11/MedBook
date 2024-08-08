f7c167c269dd589a815ceb747c6c681dca7eb82c
ajith-triffy <144329260+ajith-triffy@users.noreply.github.com>
2024-08-08 17:27:05 +0530

    Initial Commit

---
+++ MedBook/MedBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedBookApp.swift
+//  MedBook
+//
+//  Created by Ajithmadhan Krishnamoorthy on 08/08/24.
+//
+
+import SwiftUI
+
+@main
+struct MedBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
